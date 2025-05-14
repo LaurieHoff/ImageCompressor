87ef893595291c144342b9a9378dedabaddb374a
LaurieHoff <ggoodivise@hotmail.com>
2025-05-14 17:47:23 +0800

    Initial project setup
    Set up basic React TypeScript structure with:
    - Package.json with dependencies
    - TypeScript configuration
    - Basic React app skeleton
    - Initial CSS styling

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Image Compressor</h1>
+        <p>Compress your images with ease</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
